1425b62df026253add7924f5b3c3a410234ba6fa
Marco Velan <marco.velan10@gmail.com>
2022-01-07 15:14:35 +0000

    7/1/2022 TT: 3 h

--- get-mymcd-data.py
+++ get-mymcd-data.py
@@ -13,24 +13,64 @@ currentDateTime = datetime.datetime.now()
 year = int(currentDateTime.year)
 month = int(currentDateTime.month)
 day = int(currentDateTime.day)
+unused_first_day_or_whatever, actual_month_days = monthrange(year, month)
+
+# Variables and other items used in crucial functions
+shifts = []
+shift = {}
+shifts_planned_upwards = 14
+table_item_xpath = '/html/body/div[2]/div[3]/div[2]/div/table[1]/tbody/tr[3]/td[{0}]/div[{1}]'
 
 # Information to log in
 f = open('login.json')
 login = json.load(f)
 f.close
 
-# Set path Selenium
+# Set path for Selenium
 CHROMIUM_PATH = '/usr/local/bin/chromium-browser'
 s = Service(CHROMIUM_PATH)
 WINDOW_SIZE = "1920,1080"
 
-# Options
+# Options Selenium
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = CHROMIUM_PATH
 driver = webdriver.Chrome(options=chrome_options)
+
+# Function that simplifies the getting shifts data function
+def gettingTableData(starting_day, ammount_of_days):
+
+    for i in range(starting_day + 1, ammount_of_days):
+
+            shift = {
+                'day': i - 1,
+                'start': driver.find_element_by_xpath(table_item_xpath.format(i, 1)).text,
+                'end': driver.find_element_by_xpath(table_item_xpath.format(i, 2)).text,
+                'note': driver.find_element_by_xpath(table_item_xpath.format(i, 3)).text
+            }
+            shifts.append(shift)
+
+# Function to get shifts two weeks upfront
+def gettingTheMonthlyData(days_in_the_month, day):
+    
+    if days_in_the_month - day >= shifts_planned_upwards:
+
+        gettingTableData(day, shifts_planned_upwards)
+
+    else:
+
+        days_left_in_month = days_in_the_month - day
+        days_left = shifts_planned_upwards - days_left_in_month
+
+        gettingTableData(day, days_left_in_month)
+        driver.get("https://mymcd.eu/app/CZ019/#/shifts/{0}-{1}/".format(year, month + 1))
+        time.sleep(1)
+        gettingTableData(day, days_left_in_month)
+
+    with open('shifts.json', 'w', encoding='utf-8') as f:
+        json.dump(shifts, f, ensure_ascii=False, indent=4)
 
 # Login section
 driver.get("https://mymcd.eu/login/")
@@ -41,9 +81,5 @@ driver.find_element_by_xpath('/html/body/div[1]/form/div[2]/input[2]').click()
 # Getting the shifts data
 driver.get("https://mymcd.eu/app/CZ019/#/shifts/")
 time.sleep(1)
-actual_month_days = monthrange(year, month)
-
+gettingTheMonthlyData(actual_month_days, day)
 driver.close()
-
-#Function to get shifts two weeks upfront
-#def gettingTheMonthlyData(monthrange, day):
